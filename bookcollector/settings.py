cb98535be16e25b84876a8d83f3121b68248adbc
Samuel Trahan <samtrahan91@gmail.com>
2020-06-16 20:56:18 -0500

    basic setup

--- bookcollector/settings.py
+++ bookcollector/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'bookcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookcollector',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
