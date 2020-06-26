28025b3a65259aff9e3c37806fce99a3dd9c831e
Samuel Trahan <samtrahan91@gmail.com>
2020-06-26 08:19:19 -0500

    read me update

--- bookcollector/settings.py
+++ bookcollector/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,7 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import django_heroku
 django_heroku.settings(locals())
 WSGI_APPLICATION = 'bookcollector.wsgi.application'
-
